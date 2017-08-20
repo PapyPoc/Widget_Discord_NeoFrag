2e5c5659147da3adeff8c382efd7ba8b677ab674
Papy Poc <stephane.morin4@orange.fr>
2017-08-20 16:36:48 +0200

    Delete footer.tpl.php

--- views/footer.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php
-	echo '<span class="wdiscord-footer-info"><?php echo $discord->name;?></span>';
-if ($this->user()){
-	echo '<a title="Connexion" class="wdiscord-btn-connect" href="https://discord.gg/'<?php echo $data['connect'];?>'" target="_blank">'<?php echo $discord->name;?>'</a>';
-} else {
-	echo '<a class="wdiscord-btn-connect">'<?php echo $discord->name;?>'</a>';
-}
-	echo '</span>';
-?>
