5bfd5e8b818143a3a842f40e915c42488091e1a5
Papy Poc <stephane.morin4@orange.fr>
2018-10-12 12:23:13 +0200

    Version 2.0

--- controllers/index.php
+++ controllers/index.php
@@ -1,21 +1,6 @@
 <?php if (!defined('NEOFRAG_CMS')) exit;
 /**************************************************************************
-Copyright © 2015 Michaël BILCOT & Jérémy VALENTIN
-
-This file is part of NeoFrag.
-
-NeoFrag is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-NeoFrag is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License
-along with NeoFrag. If not, see <http://www.gnu.org/licenses/>.
+Copyright © 2018 Morin Stéphane
 **************************************************************************/
 
 class w_discord_c_index extends Controller_Widget
@@ -32,6 +17,6 @@ class w_discord_c_index extends Controller_Widget
 }
 
 /*
-NeoFrag Alpha 0.1.6
+NeoFrag Alpha 0.2.1
 ./widgets/discord/controllers/index.php
 */
