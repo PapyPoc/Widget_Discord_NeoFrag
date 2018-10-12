0526b0ef1fad47d7bacf7c858a076321752a5775
Papy Poc <stephane.morin4@orange.fr>
2018-10-12 12:24:46 +0200

    Version 2.0

--- lang/fr.php
+++ lang/fr.php
@@ -1,24 +1,5 @@
 <?php if (!defined('NEOFRAG_CMS')) exit;
 /**************************************************************************
-Copyright © 2015 Michaël BILCOT & Jérémy VALENTIN
-
-This file is part of NeoFrag.
-
-NeoFrag is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-NeoFrag is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License
-along with NeoFrag. If not, see <http://www.gnu.org/licenses/>.
-**************************************************************************/
-
-/**************************************************************************
 Translated by NeoFrag community, contributors are:
 Papy Poc
 **************************************************************************/
@@ -29,6 +10,6 @@ $lang['ligne']		= 'Membre en ligne:';
 $lang['connected']	= 'Connexion';
 
 /*
-NeoFrag Alpha 0.1.6
+NeoFrag Alpha 0.2.1
 ./widgets/discord/lang/en.php
 */
