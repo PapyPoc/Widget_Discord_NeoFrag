34e0ba429bcf883dbaf3d2519de956b643a9b18a
Papy_Poc <stephane.morin4@orange.fr>
2018-10-25 14:58:57 +0200

    Ajout de l'anglais

---
+++ langs/en.php
@@ -0,0 +1,18 @@
+<?php 
+/**
+ * https://translate.neofr.ag
+ * @author: Papy Poc
+ */
+
+return [
+	'Discord'	=> 'Discord'
+	'member'	=> 'member connected'
+	'members'	=> 'members connected'
+	'ligne'		=> 'Online Member:'
+	'connected'	=> 'Connect'
+	'id_serveur'=> 'Server ID'
+	'invite'	=> 'Server invitation'
+	'options'	=> 'Options'
+	'color'		=> 'Color (dark ou light)'
+	'height'	=> 'Height of the Discord Widget'
+];
